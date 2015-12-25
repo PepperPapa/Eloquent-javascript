fbed31012406cc3d4d9629decdc81d20a0dfd79e
pepperpapa <zhongxin2506@outlook.com>
2015-12-25 17:07:13 +0800

    add descriptions for charter7 exercises

--- ch7-ex1-retry.js
+++ ch7-ex1-retry.js
@@ -1,3 +1,12 @@
+/*
+Retry
+Say you have a function primitiveMultiply that, in 50 percent of cases,
+multiplies two numbers, and in the other 50 percent, raises an exception
+of type MultiplicatorUnitFailure. Write a function that wraps this clunky
+function and just keeps trying until a call succeeds, after which it returns
+the result.
+Make sure you handle only the exceptions you are trying to handle.
+*/
 function MultiplicatorUnitFailure() {
   this.stack = (new Error()).stack;
 }
--- ch7-ex1-the-locked-box.js
+++ ch7-ex1-the-locked-box.js
@@ -1,3 +1,25 @@
+/*
+The locked box
+Consider the following (rather contrived) object:
+var box = {
+l o c k e d : true ,
+u n l o c k : f u n c t i o n () { this . l o c k e d = false ; } ,
+lock : f u n c t i o n () { this . l o c k e d = true ; } ,
+_ c o n t e n t : [] ,
+get c o n t e n t () {
+if ( this . l o c k e d ) throw new Error (" L o c k e d !") ;
+r e t u r n this . _ c o n t e n t ;
+}
+};
+It is a box with a lock. Inside is an array, but you can get at it only
+when the box is unlocked. Directly accessing the _content property is not
+allowed.
+Write a function called withBoxUnlocked that takes a function value as
+argument, unlocks the box, runs the function, and then ensures that the
+box is locked again before returning, regardless of whether the argument
+function returned normally or threw an exception.
+*/
+
 function withBoxUnlocked(body) {
   //unlock the box
   box.unlock();
